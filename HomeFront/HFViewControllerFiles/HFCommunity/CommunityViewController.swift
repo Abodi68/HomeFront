3c0560fbea209ddc0c6f5032a5ccc71e36007695
Alexander Bodi <abodi1@live.maryville.edu>
2025-09-28 22:04:03 -0500

    No major changes, other than adding a map to the community page and changing some text labels.

--- HomeFront/HFViewControllerFiles/HFCommunity/CommunityViewController.swift
+++ HomeFront/HFViewControllerFiles/HFCommunity/CommunityViewController.swift
@@ -6,24 +6,25 @@
 //
 
 import UIKit
+import MapKit
 
 class CommunityViewController: BaseViewController {
 
     private lazy var titleLabel = createLabel(text: "Community", fontSize: 28, bold: true)
-    private lazy var joinButton = createPrimaryButton(title: "Join Discussion")
+    private lazy var eventLabel = createLabel(text: "Events and Centers Closest to You", fontSize: 20, bold: false)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(titleLabel)
-        view.addSubview(joinButton)
+        view.addSubview(eventLabel)
 
         NSLayoutConstraint.activate([
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
 
-            joinButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            joinButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
-            joinButton.widthAnchor.constraint(equalToConstant: 160)
+            eventLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            eventLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            eventLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8)
         ])
     }
 }
